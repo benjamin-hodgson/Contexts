5e98b4bc04ca7dac289c39c3e3d7a84ed0b069af
Benjamin Hodgson <poorsod@gmail.com>
2013-09-29 16:23:31 +0100

    Added alternative names

--- src/pyspec.py
+++ src/pyspec.py
@@ -1,27 +1,39 @@
 import inspect
+import re
+
+establish_re = re.compile(r"(^|_)([Ee]stablish|[Cc]ontext)")
+because_re = re.compile(r"(^|_)([Bb]ecause|[Ww]hen|[Ss]ince|[Aa]fter)")
+should_re = re.compile(r"(^|_)([Ss]hould|[Ii]t|[Mm]ust|[Ww]ill)")
+cleanup_re = re.compile(r"(^|_)([Cc]leanup)")
 
 
 def no_op():
     pass
 
+
+class Spec(object):
+    def __init__(self, spec):
+        self.establish = no_op
+        self.because = no_op
+        self.shoulds = []
+        self.cleanup = no_op
+        for name, meth in inspect.getmembers(spec, inspect.ismethod):
+            if re.search(establish_re, name):
+                self.establish = meth
+            if re.search(because_re, name):
+                self.because = meth
+            if re.search(should_re, name):
+                self.shoulds.append(meth)
+            if re.search(cleanup_re, name):
+                self.cleanup = meth
+
+    def run(self):
+        self.establish()
+        self.because()
+        for should in self.shoulds:
+            should()
+        self.cleanup()
+
+
 def run(spec):
-    establish = no_op
-    because = no_op
-    shoulds = []
-    cleanup = no_op
-
-    for name, meth in inspect.getmembers(spec, inspect.ismethod):
-        if "establish" in name:
-            establish = meth
-        if "because" in name:
-            because = meth
-        if "should" in name:
-            shoulds.append(meth)
-        if "cleanup" in name:
-            cleanup = meth
-
-    establish()
-    because()
-    for should in shoulds:
-        should()
-    cleanup()
+    Spec(spec).run()
--- test/spec_specs.py
+++ test/spec_specs.py
@@ -24,6 +24,7 @@ class WhenWeRunASpec(object):
     def it_should_run_the_methods_in_the_correct_order(self):
         self.spec.log.should.equal("arrange act assert teardown ")
 
+
 class WhenWeRunASpecWithMultipleAssertionsWithoutAnEstablishClause(object):
     def establish_the_spec(self):
         class SpecWithoutEstablishOrCleanup(object):
@@ -43,6 +44,48 @@ class WhenWeRunASpecWithMultipleAssertionsWithoutAnEstablishClause(object):
         self.spec.assertions.should.equal(2)
 
 
+class WhenWeRunSpecsWithAlternatelyNamedMethods(object):
+    def establish_the_specs(self):
+        class AlternatelyNamedMethods(object):
+            def __init__(self):
+                self.log = ""
+            def context(self):
+                self.log += "arrange "
+            def has_when_in_the_name(self):  # to prevent collision with Sure
+                self.log += "act "
+            def it(self):
+                self.log += "assert "
+        class MoreAlternativeNames(object):
+            def __init__(self):
+                self.log = ""
+            def since(self):
+                self.log += "act "
+            def has_must_in_the_name(self):
+                self.log += "assert "
+        class EvenMoreAlternativeNames(object):
+            def __init__(self):
+                self.log = ""
+            def after(self):
+                self.log += "act "
+            def will(self):
+                self.log += "assert "
+
+        self.spec1 = AlternatelyNamedMethods()
+        self.spec2 = MoreAlternativeNames()
+        self.spec3 = EvenMoreAlternativeNames()
+
+    def because_we_run_the_specs(self):
+        pyspec.run(self.spec1)
+        pyspec.run(self.spec2)
+        pyspec.run(self.spec3)
+
+    def it_should_run_the_methods_in_the_correct_order(self):
+        self.spec1.log.should.equal("arrange act assert ")
+        self.spec2.log.should.equal("act assert ")
+        self.spec3.log.should.equal("act assert ")
+
+
 if __name__ == "__main__":
     pyspec.run(WhenWeRunASpec())
     pyspec.run(WhenWeRunASpecWithMultipleAssertionsWithoutAnEstablishClause())
+    pyspec.run(WhenWeRunSpecsWithAlternatelyNamedMethods())
