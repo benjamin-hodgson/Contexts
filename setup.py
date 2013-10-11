87b7a0789e5b45ffad63abd0371e9426e011574b
Benjamin Hodgson <poorsod@gmail.com>
2013-10-11 21:30:27 +0100

    minor setup.py improvements

--- setup.py
+++ setup.py
@@ -1,11 +1,14 @@
-from setuptools import setup
+import ez_setup
+ez_setup.use_setuptools()
+from setuptools import setup, find_packages
 
 setup(
     name = "pyspec",
     version = "0.0.1",
-    package_dir = {'':"src"},
-    packages = [
-        "pyspec",
-    ],
+    author = "Benjamin Hodgson",
+    author_email = "benjamin.hodgson@huddle.net",
+    url = "https://github.com/benjamin-hodgson/PySpec",
+    package_dir = {'':'src'},
+    packages = find_packages('src'),
     tests_require = ["sure >= 1.2.2"]
 )
