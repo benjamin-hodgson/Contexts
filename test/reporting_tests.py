690a1379faa903626f836973581cd0df4ef5d228
Benjamin Hodgson <benjamin.hodgson@huddle.com>
2013-10-22 15:41:01 +0100

    Streamlined the reporting tests

--- test/reporting_tests.py
+++ test/reporting_tests.py
@@ -47,21 +47,20 @@ class WhenWatchingForDots(object):
 class WhenPrintingASuccessfulResult(object):
     def in_the_context_of_a_successful_run(self):
         # We don't want it to try and print anything while we set it up
-        self.result = reporting.TextResult(StringIO())
-        with self.result.run_context(None):
-            with self.result.run_assertion(None):
-                pass
-            with self.result.run_assertion(None):
-                pass
-        with self.result.run_context(None):
-            with self.result.run_assertion(None):
-                pass
-
         self.stringio = StringIO()
-        self.result.stream = self.stringio
+        self.result = reporting.TextResult(StringIO())
 
-    def because_we_print_the_summary(self):
-        self.result.summarise()
+    def because_we_run_some_tests(self):
+        with self.result.run_suite(None):
+            with self.result.run_context(None):
+                with self.result.run_assertion(None):
+                    pass
+                with self.result.run_assertion(None):
+                    pass
+            with self.result.run_context(None):
+                with self.result.run_assertion(None):
+                    pass
+            self.result.stream = self.stringio
 
     def it_should_print_the_summary_to_the_stream(self):
         self.stringio.getvalue().should.equal(
@@ -74,34 +73,31 @@ PASSED!
 class WhenPrintingAFailureResult(object):
     def in_the_context_of_a_failed_run(self):
         self.result = reporting.TextResult(StringIO())
-
-        exception1 = TypeError("Gotcha")
-        tb1 = [('made_up_file.py', 3, 'made_up_function', 'frame1'),
-               ('another_made_up_file.py', 2, 'another_made_up_function', 'frame2')]
-        assertion1 = contexts.core.Assertion(None, "made.up.assertion_1")
-
-        exception2 = AssertionError("you fail")
-        tb2 = [('made_up_file_3.py', 1, 'made_up_function_3', 'frame3'),
-               ('made_up_file_4.py', 2, 'made_up_function_4', 'frame4')]
-        assertion2 = contexts.core.Assertion(None, "made.up.assertion_2")
-
-        exception3 = ZeroDivisionError("oh dear")
-        tb3 = [('made_up_file_4.py', 1, 'made_up_function_4', 'frame4'),
-               ('made_up_file_5.py', 2, 'made_up_function_5', 'frame5')]
-        context3 = contexts.core.Context([],[],[],[],"made.up_context")
-
-        with self.result.run_context(None):
-            # Figure out a way to do this using the context manager?
-            self.result.assertion_errored(assertion1, exception1, tb1)
-            self.result.assertion_failed(assertion2, exception2, tb2)
-
-        self.result.context_errored(context3, exception3, tb3)
-
         self.stringio = StringIO()
-        self.result.stream = self.stringio
 
-    def because_we_print_the_summary(self):
-        self.result.summarise()
+        self.exception1 = TypeError("Gotcha")
+        self.tb1 = [('made_up_file.py', 3, 'made_up_function', 'frame1'),
+               ('another_made_up_file.py', 2, 'another_made_up_function', 'frame2')]
+        self.assertion1 = contexts.core.Assertion(None, "made.up.assertion_1")
+
+        self.exception2 = AssertionError("you fail")
+        self.tb2 = [('made_up_file_3.py', 1, 'made_up_function_3', 'frame3'),
+               ('made_up_file_4.py', 2, 'made_up_function_4', 'frame4')]
+        self.assertion2 = contexts.core.Assertion(None, "made.up.assertion_2")
+
+        self.exception3 = ZeroDivisionError("oh dear")
+        self.tb3 = [('made_up_file_4.py', 1, 'made_up_function_4', 'frame4'),
+               ('made_up_file_5.py', 2, 'made_up_function_5', 'frame5')]
+        self.context3 = contexts.core.Context([],[],[],[],"made.up_context")
+
+    def because_we_run_some_tests(self):
+        with self.result.run_suite(None):
+            with self.result.run_context(None):
+                # Figure out a way to do this using the context manager?
+                self.result.assertion_errored(self.assertion1, self.exception1, self.tb1)
+                self.result.assertion_failed(self.assertion2, self.exception2, self.tb2)
+            self.result.context_errored(self.context3, self.exception3, self.tb3)
+            self.result.stream = self.stringio
 
     def it_should_print_a_traceback_for_each_failure(self):
         self.stringio.getvalue().should.equal(
@@ -149,15 +145,12 @@ class WhenTimingATestRun(object):
         self.FakeDateTime = FakeDateTime
 
         self.stringio = StringIO()
-        self.result = reporting.TimedTextResult(StringIO())
+        self.result = reporting.TimedTextResult(self.stringio)
 
     def because_we_run_a_suite(self):
         with mock.patch('datetime.datetime', self.FakeDateTime):
             with self.result.run_suite(None):
                 datetime.datetime.now.return_value += self.fake_soon
-
-        self.result.stream = self.stringio
-        self.result.summarise()
 
     def it_should_report_the_total_time_for_the_test_run(self):
         self.stringio.getvalue().should.equal("""
