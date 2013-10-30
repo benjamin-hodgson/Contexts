ec41bfd8f5eeeb097ab425d18be131371936cd0d
Benjamin Hodgson <benjamin.hodgson@huddle.com>
2013-10-30 10:20:05 +0000

    Docstrings for the two main public API functions

--- src/contexts/__init__.py
+++ src/contexts/__init__.py
@@ -8,6 +8,9 @@ __all__ = ['run', 'main', 'catch']
 
 
 def main():
+    """
+    Find and run any test classes in the main file.
+    """
     passed = run()
     if not passed:
         sys.exit(1)
@@ -32,6 +35,18 @@ def _run_impl(spec, result):
 
 
 def catch(func, *args, **kwargs):
+    """
+    Call the supplied function with the supplied arguments,
+    catching and returning any exception that it throws.
+
+    Arguments:
+        func: the function to run.
+        *args: positional arguments to pass into the function.
+        **kwargs: keyword arguments to pass into the function.
+    Returns:
+        If the function throws an exception, return the exception.
+        If the function does not throw an exception, return None.
+    """
     try:
         func(*args, **kwargs)
     except Exception as e:
