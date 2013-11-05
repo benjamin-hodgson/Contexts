4853dccd11f242f6e7b7d288324697c6e4afb13d
Benjamin Hodgson <benjamin.hodgson@huddle.com>
2013-11-05 10:31:33 +0000

    Refactoring Result to use a more natural internal representation

--- src/contexts/reporting.py
+++ src/contexts/reporting.py
@@ -38,41 +38,88 @@ class Result(object):
         """Called when an assertion throws an AssertionError"""
 
 
+class ContextViewModel(object):
+    def __init__(self, context):
+        self.name = context.name
+        self.assertions = []
+        self._exception = None
+        self.error_summary = None
+
+    @property
+    def exception(self):
+        return self._exception
+    @exception.setter
+    def exception(self, value):
+        self._exception = value
+        self.error_summary = format_exception(value)
+
+    @property
+    def assertion_failures(self):
+        return [a for a in self.assertions if a.status == "failed"]
+    @property
+    def assertion_errors(self):
+        return [a for a in self.assertions if a.status == "errored"]
+
+
+class AssertionViewModel(object):
+    def __init__(self, assertion, status="passed", exception=None):
+        self.name = assertion.name
+        self.status = status
+        self.error_summary = None
+        if exception is not None:
+            self.error_summary = format_exception(exception)
+
+
 class SimpleResult(Result):
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
-        self.contexts = []
-        self.assertions = []
-        self.context_errors = []
-        self.assertion_errors = []
-        self.assertion_failures = []
+        self.view_models = []
 
     @property
     def failed(self):
         return self.context_errors or self.assertion_errors or self.assertion_failures
-
-    def assertion_started(self, assertion):
-        super().assertion_started(assertion)
-        self.assertions.append(assertion)
-
-    def assertion_failed(self, assertion, exception):
-        super().assertion_failed(assertion, exception)
-        self.assertion_failures.append((assertion, exception))
-        exception.__traceback__ = None
-
-    def assertion_errored(self, assertion, exception):
-        super().assertion_errored(assertion, exception)
-        self.assertion_errors.append((assertion, exception))
-        exception.__traceback__ = None
+    @property
+    def assertions(self):
+        return [a for vm in self.view_models for a in vm.assertions]
+    @property
+    def assertion_failures(self):
+        return [a for a in self.assertions if a.status == "failed"]
+    @property
+    def assertion_errors(self):
+        return [a for a in self.assertions if a.status == "errored"]
+    @property
+    def contexts(self):
+        return self.view_models
+    @property
+    def context_errors(self):
+        return [vm for vm in self.view_models if vm.error_summary is not None]
 
     def context_started(self, context):
         super().context_started(context)
-        self.contexts.append(context)
+        self.view_models.append(ContextViewModel(context))
 
     def context_errored(self, context, exception):
+        context_vm = self.view_models[-1]
+        context_vm.exception = exception
         super().context_errored(context, exception)
-        self.context_errors.append((context, exception))
-        exception.__traceback__ = None
+
+    def assertion_passed(self, assertion):
+        context_vm = self.view_models[-1]
+        assertion_vm = AssertionViewModel(assertion)
+        context_vm.assertions.append(assertion_vm)
+        super().assertion_passed(assertion)
+
+    def assertion_failed(self, assertion, exception):
+        context_vm = self.view_models[-1]
+        assertion_vm = AssertionViewModel(assertion, "failed", exception)
+        context_vm.assertions.append(assertion_vm)
+        super().assertion_failed(assertion, exception)
+
+    def assertion_errored(self, assertion, exception):
+        context_vm = self.view_models[-1]
+        assertion_vm = AssertionViewModel(assertion, "errored", exception)
+        context_vm.assertions.append(assertion_vm)
+        super().assertion_errored(assertion, exception)
 
 
 class StreamResult(Result):
@@ -173,112 +220,51 @@ def pluralise(noun, num):
     return string
 
 
-class ContextViewModel(object):
-    def __init__(self, context):
-        self.name = context.name
-        self.assertions = []
-        self._exception = None
-        self.error_summary = None
-
-    @property
-    def exception(self):
-        return self._exception
-    @exception.setter
-    def exception(self, value):
-        self._exception = value
-        self.error_summary = format_exception(value)
-
-    @property
-    def assertion_failures(self):
-        return [a for a in self.assertions if a.status == "failed"]
-    @property
-    def assertion_errors(self):
-        return [a for a in self.assertions if a.status == "errored"]
-
-
-class AssertionViewModel(object):
-    def __init__(self, assertion, status="passed", exception=None):
-        self.name = assertion.name
-        self.status = status
-        self.error_summary = None
-        if exception is not None:
-            self.error_summary = format_exception(exception)
-
-
-class SimpleHierarchicalResult(Result):
-    def __init__(self, *args, **kwargs):
-        super().__init__(*args, **kwargs)
-        self.view_models = []
-
-    @property
-    def assertions(self):
-        return [a for vm in self.view_models for a in vm.assertions]
-    @property
-    def assertion_failures(self):
-        return [a for a in self.assertions if a.status == "failed"]
-    @property
-    def assertion_errors(self):
-        return [a for a in self.assertions if a.status == "errored"]
-    @property
-    def context_errors(self):
-        return [vm for vm in self.view_models if vm.error_summary is not None]
-
-    def context_started(self, context):
-        super().context_started(context)
-        self.view_models.append(ContextViewModel(context))
-
-    def context_errored(self, context, exception):
-        context_vm = self.view_models[-1]
-        context_vm.exception = exception
-        super().context_errored(context, exception)
-
-    def assertion_passed(self, assertion):
-        context_vm = self.view_models[-1]
-        assertion_vm = AssertionViewModel(assertion)
-        context_vm.assertions.append(assertion_vm)
-        super().assertion_passed(assertion)
-
-    def assertion_failed(self, assertion, exception):
-        context_vm = self.view_models[-1]
-        assertion_vm = AssertionViewModel(assertion, "failed", exception)
-        context_vm.assertions.append(assertion_vm)
-        super().assertion_failed(assertion, exception)
-
-    def assertion_errored(self, assertion, exception):
-        context_vm = self.view_models[-1]
-        assertion_vm = AssertionViewModel(assertion, "errored", exception)
-        context_vm.assertions.append(assertion_vm)
-        super().assertion_errored(assertion, exception)
-
-
-class HierarchicalResult(SimpleHierarchicalResult, StreamResult):
+class HierarchicalResult(SimpleResult, StreamResult):
     dashes = '-' * 70
 
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
         self.summary = []
 
+    def context_started(self, context):
+        super().context_started(context)
+        self.current_summary = [context.name]
+
+    def context_ended(self, context):
+        super().context_ended(context)
+        context_vm = self.view_models[-1]
+        if context_vm.assertion_failures or context_vm.assertion_errors:
+            self.summary.extend(self.current_summary)
+
+    def context_errored(self, context, exception):
+        super().context_errored(context, exception)
+        context_vm = self.view_models[-1]
+        formatted_exc = ''.join(context_vm.error_summary).split('\n')[:-1]
+        self.current_summary.extend('  ' + s for s in formatted_exc)
+        self.summary.extend(self.current_summary)
+
+    def assertion_failed(self, assertion, exception):
+        super().assertion_failed(assertion, exception)
+        context_vm = self.view_models[-1]
+        assertion_vm = context_vm.assertions[-1]
+        formatted_exc = ''.join(assertion_vm.error_summary).split('\n')[:-1]
+        self.current_summary.append('  FAIL: ' + assertion_vm.name)
+        self.current_summary.extend('    ' + s for s in formatted_exc)
+
+    def assertion_errored(self, assertion, exception):
+        super().assertion_errored(assertion, exception)
+        context_vm = self.view_models[-1]
+        assertion_vm = context_vm.assertions[-1]
+        formatted_exc = ''.join(assertion_vm.error_summary).split('\n')[:-1]
+        self.current_summary.append('  ERROR: ' + assertion_vm.name)
+        self.current_summary.extend('    ' + s for s in formatted_exc)
+
     def suite_ended(self, suite):
         super().suite_ended(suite)
         self.summarise()
 
     def summarise(self):
-        for context_vm in self.view_models:
-            if not context_vm.error_summary and not context_vm.assertion_errors and not context_vm.assertion_failures:
-                continue
-            self.summary.append(context_vm.name)
-            for assertion_vm in context_vm.assertion_errors:
-                formatted_exc = ''.join(assertion_vm.error_summary).split('\n')[:-1]
-                self.summary.append('  ERROR: ' + assertion_vm.name)
-                self.summary.extend('    ' + s for s in formatted_exc)
-            for assertion_vm in context_vm.assertion_failures:
-                formatted_exc = ''.join(assertion_vm.error_summary).split('\n')[:-1]
-                self.summary.append('  FAIL: ' + assertion_vm.name)
-                self.summary.extend('    ' + s for s in formatted_exc)
-            if context_vm.error_summary:
-                formatted_exc = ''.join(context_vm.error_summary).split('\n')[:-1]
-                self.summary.extend('  ' + s for s in formatted_exc)
-
         self._print('')
         self._print(self.dashes)
         if self.failed:
@@ -354,6 +340,37 @@ class CapturingResult(SummarisingResult):
             self.summary.append("--------------------- >> end captured stdout << ----------------------")
 
 
+# class HierarchicalCapturingResult(HierarchicalResult):
+#     def context_started(self, context):
+#         super().context_started(context)
+#         self.real_stdout = sys.stdout
+#         self.buffer = StringIO()
+#         sys.stdout = self.buffer
+
+#     def context_ended(self, context):
+#         sys.stdout = self.real_stdout
+#         super().context_ended(context)
+
+#     def context_errored(self, context, exception):
+#         sys.stdout = self.real_stdout
+#         super().context_errored(context, exception)
+#         self.append_buffer_to_summary()
+
+#     def assertion_failed(self, assertion, exception):
+#         super().assertion_failed(assertion, exception)
+#         self.append_buffer_to_summary()
+
+#     def assertion_errored(self, assertion, exception):
+#         super().assertion_errored(assertion, exception)
+#         self.append_buffer_to_summary()
+
+#     def append_buffer_to_summary(self):
+#         if self.buffer.getvalue():
+#             self.summary.append("-------------------- >> begin captured stdout << ---------------------")
+#             self.summary.append(self.buffer.getvalue()[:-1])
+#             self.summary.append("--------------------- >> end captured stdout << ----------------------")
+
+
 class NonCapturingCLIResult(DotsResult, TimedResult, SummarisingResult):
     pass
 
@@ -367,4 +384,6 @@ class HierarchicalCLIResult(DotsResult, TimedResult, HierarchicalResult):
 
 
 def format_exception(exception):
-    return traceback.format_exception(type(exception), exception, exception.__traceback__)
+    ret = traceback.format_exception(type(exception), exception, exception.__traceback__)
+    exception.__traceback__ = None
+    return ret
