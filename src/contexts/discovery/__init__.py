632738c8905ff38f8caa82bb3a11f3c1883c35de
Benjamin Hodgson <benjamin.hodgson@huddle.com>
2013-11-12 15:46:15 +0000

    Inlined importers.py

--- src/contexts/discovery/__init__.py
+++ src/contexts/discovery/__init__.py
@@ -1,8 +1,10 @@
+import contextlib
+import importlib
 import itertools
 import os
 import re
+import sys
 from collections import namedtuple
-from .importers import load_module
 
 
 PackageSpecification = namedtuple('PackageSpecification', ['parent_folder', 'package_name'])
@@ -13,15 +15,52 @@ folder_re = re.compile(r"([Ss]pec|[Tt]est)")
 
 
 def import_from_file(file_path):
+    """
+    Import the specified file, with an unqualified module name.
+    """
     folder = os.path.dirname(file_path)
     filename = os.path.basename(file_path)
     module_name = os.path.splitext(filename)[0]
     return load_module(folder, module_name)
 
 
+def load_module(dir_path, module_name):
+    """
+    Load specified module from the specified directory
+    """
+    prune_sys_dot_modules(dir_path, module_name)
+    with prepend_folder_to_sys_dot_path(dir_path):
+        return importlib.import_module(module_name)
+
+
 def find_modules(directory):
+    """
+    Return an iterable of ModuleSpecifications found in the folder
+    """
     finders = FinderGenerator(directory).generate_finders()
     return itertools.chain.from_iterable(finder.find_modules() for finder in finders)
+
+
+def prune_sys_dot_modules(dir_path, module_name):
+    requested_file = os.path.join(dir_path, module_name + '.py')
+    if module_name in sys.modules:
+        existing_module = sys.modules[module_name]
+        if not same_file(existing_module.__file__, requested_file):
+            del sys.modules[module_name]
+            importlib.invalidate_caches()
+
+
+@contextlib.contextmanager
+def prepend_folder_to_sys_dot_path(folder_path):
+    sys.path.insert(0, folder_path)
+    try:
+        yield
+    finally:
+        sys.path.pop(0)
+
+
+def same_file(path1, path2):
+    return os.path.realpath(path1) == os.path.realpath(path2)
 
 
 class FinderGenerator(object):
