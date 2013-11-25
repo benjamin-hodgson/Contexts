72db23d3f4982523b7cfd3de2d8d01d0afea2a8f
Benjamin Hodgson <poorsod@gmail.com>
2013-11-25 22:49:01 +0000

    Replaced SummarisingReporter with the new implementation altogether

--- src/contexts/reporting/cli.py
+++ src/contexts/reporting/cli.py
@@ -26,112 +26,6 @@ class DotsReporter(shared.StreamReporter):
         self._print('E', end='')
 
 
-class SummarisingReporter(shared.SimpleReporter, shared.StreamReporter):
-    dashes = '-' * 70
-
-    def __init__(self, *args, **kwargs):
-        super().__init__(*args, **kwargs)
-        self.summary = []
-        self.current_summary = None
-        self.current_indent = ''
-
-    def context_started(self, context):
-        super().context_started(context)
-        self.current_summary = [self.suite_view_model.contexts[context].name]
-        self.indent()
-
-    def context_ended(self, context):
-        super().context_ended(context)
-        ctx = self.suite_view_model.contexts[context]
-        if ctx.assertion_failures or ctx.assertion_errors:
-            self.add_current_context_to_summary()
-        self.dedent()
-        self.current_summary = None
-
-    def context_errored(self, context, exception):
-        super().context_errored(context, exception)
-        ctx = self.suite_view_model.contexts[context]
-        self.extend_summary(ctx.error_summary)
-        self.add_current_context_to_summary()
-        self.dedent()
-        self.current_summary = None
-
-    def assertion_failed(self, assertion, exception):
-        super().assertion_failed(assertion, exception)
-        self.add_assertion_to_summary(assertion)
-
-    def assertion_errored(self, assertion, exception):
-        super().assertion_errored(assertion, exception)
-        self.add_assertion_to_summary(assertion)
-
-    def suite_ended(self, suite):
-        super().suite_ended(suite)
-        self.summarise()
-
-    def unexpected_error(self, exception):
-        super().unexpected_error(exception)
-        formatted_exc = self.suite_view_model.unexpected_errors[-1]
-        self.extend_summary(formatted_exc)
-
-    def indent(self):
-        self.current_indent += '  '
-
-    def dedent(self):
-        self.current_indent = self.current_indent[:-2]
-
-    def append_to_summary(self, string):
-        if self.current_summary is not None:
-            self.current_summary.append(self.current_indent + string)
-        else:
-            self.summary.append(self.current_indent + string)
-
-    def extend_summary(self, iterable):
-        if self.current_summary is not None:
-            self.current_summary.extend(self.current_indent + s for s in iterable)
-        else:
-            self.summary.extend(self.current_indent + s for s in iterable)
-
-    def add_current_context_to_summary(self):
-        self.summary.extend(self.current_summary)
-
-    def add_assertion_to_summary(self, assertion):
-        assertion_vm = self.suite_view_model.current_context.assertions[assertion]
-        formatted_exc = assertion_vm.error_summary
-
-        if assertion_vm.status == "errored":
-            self.append_to_summary('ERROR: ' + assertion_vm.name)
-        elif assertion_vm.status == "failed":
-            self.append_to_summary('FAIL: ' + assertion_vm.name)
-
-        self.indent()
-        self.extend_summary(formatted_exc)
-        self.dedent()
-
-    def summarise(self):
-        self._print('')
-        self._print(self.dashes)
-        if self.suite_view_model.failed:
-            self._print('\n'.join(self.summary))
-            self._print(self.dashes)
-            self._print('FAILED!')
-            self._print(self.failure_numbers())
-        else:
-            self._print('PASSED!')
-            self._print(self.success_numbers())
-
-    def success_numbers(self):
-        return "{}, {}".format(
-            pluralise("context", len(self.suite_view_model.contexts)),
-            pluralise("assertion", len(self.suite_view_model.assertions)))
-
-    def failure_numbers(self):
-        return "{}, {}: {} failed, {}".format(
-            pluralise("context", len(self.suite_view_model.contexts)),
-            pluralise("assertion", len(self.suite_view_model.assertions)),
-            len(self.suite_view_model.assertion_failures),
-            pluralise("error", len(self.suite_view_model.assertion_errors) + len(self.suite_view_model.context_errors) + len(self.suite_view_model.unexpected_errors)))
-
-
 class VerboseReporter(shared.SimpleReporter, shared.StreamReporter):
     dashes = '-' * 70
     def __init__(self, *args, **kwargs):
@@ -214,7 +108,7 @@ class VerboseReporter(shared.SimpleReporter, shared.StreamReporter):
             pluralise("error", len(self.suite_view_model.assertion_errors) + len(self.suite_view_model.context_errors) + len(self.suite_view_model.unexpected_errors)))
 
 
-class LessVerboseReporter(VerboseReporter):
+class SummarisingReporter(VerboseReporter):
     def __init__(self, stream=sys.stdout):
         super().__init__(stream)
         self.real_stream = stream
@@ -268,19 +162,19 @@ class LessVerboseReporter(VerboseReporter):
 
 
 class StdOutCapturingReporter(SummarisingReporter):
-    def context_started(self, context):
-        super().context_started(context)
-        self.real_stdout = sys.stdout
-        self.buffer = StringIO()
-        sys.stdout = self.buffer
-
     def centred_dashes(self, string):
         num = str(70 - len(self.current_indent))
         return ("{:-^"+num+"}").format(string)
 
+    def context_started(self, context):
+        self.real_stdout = sys.stdout
+        self.buffer = StringIO()
+        sys.stdout = self.buffer
+        super().context_started(context)
+
     def context_ended(self, context):
-        super().context_ended(context)
         sys.stdout = self.real_stdout
+        super().context_ended(context)
 
     def context_errored(self, context, exception):
         super().context_errored(context, exception)
@@ -298,9 +192,9 @@ class StdOutCapturingReporter(SummarisingReporter):
     def add_buffer_to_summary(self):
         if self.buffer.getvalue():
             self.indent()
-            self.append_to_summary(self.centred_dashes(" >> begin captured stdout << "))
-            self.extend_summary(self.buffer.getvalue().strip().split('\n'))
-            self.append_to_summary(self.centred_dashes(" >> end captured stdout << "))
+            self._print(self.centred_dashes(" >> begin captured stdout << "))
+            self._print(self.buffer.getvalue().strip())
+            self._print(self.centred_dashes(" >> end captured stdout << "))
             self.dedent()
 
 
