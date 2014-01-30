799581a521e60a3eb280fe5ed8e115e481671795
Benjamin Hodgson <benjamin.hodgson@huddle.com>
2014-01-30 18:07:59 +0000

    Refactoring around plugin list builder

--- src/contexts/plugin_discovery.py
+++ src/contexts/plugin_discovery.py
@@ -1,50 +1,17 @@
 class PluginListBuilder(object):
-    # Refactoring to-do: separate out the graph-related logic
     def __init__(self):
-        self._plugins = {}
-        self._output = []
+        self.graph = Graph()
 
     def add(self, cls):
         left, right = get_location(cls)
-
-        if cls in self._plugins and self._plugins[cls]['perm_mark'] is False:
-            raise ValueError('Plugin has already been added')
-
-        self.create_entry(cls)
-        self._plugins[cls]['perm_mark'] = False
-
+        self.graph.add_node(cls)
         if right is not None:
-            self.create_entry(right)
-            self._plugins[cls]['left_of'].add(right)
-
+            self.graph.add_edge(cls, right)
         if left is not None:
-            self.create_entry(left)
-            self._plugins[left]['left_of'].add(cls)
+            self.graph.add_edge(left, cls)
 
     def to_list(self):
-        for cls in self._plugins:
-            self.visit(cls)
-        self._output.reverse()
-        return self._output
-
-    def create_entry(self, cls):
-        if cls not in self._plugins:
-            self._plugins[cls] = {'left_of':set(), 'temp_mark':False, 'perm_mark':True}
-
-    def visit(self, cls):
-        dct = self._plugins[cls]
-
-        if dct['perm_mark']:
-            return
-        if dct['temp_mark']:
-            raise ValueError('Graph has cycles')
-
-        dct['temp_mark'] = True
-        for class_further_right in dct['left_of']:
-            self.visit(class_further_right)
-
-        self._output.append(cls)
-        dct['perm_mark'] = True
+        return self.graph.topological_sort()
 
 
 def get_location(cls):
@@ -56,4 +23,61 @@ def get_location(cls):
             return location
     else:
         return (None, None)
+
+
+class Graph(object):
+    def __init__(self):
+        self.nodes = {}
+        self.output = []
+
+    def add_node(self, node):
+        if node in self.nodes and self.nodes[node]['added']:
+            raise ValueError('Node has already been added')
+
+        self.create_if_necessary(node)
+        self.nodes[node]['added'] = True
+
+    def add_edge(self, tail, head):
+        self.create_if_necessary(tail)
+        self.create_if_necessary(head)
+        self.nodes[tail]['edges_to'].add(head)
+
+    def topological_sort(self):
+        sorter = TopologicalSorter(self)
+        return sorter.sort()
+
+    def create_if_necessary(self, node):
+        if node not in self.nodes:
+            self.nodes[node] = {'edges_to':set(), 'added':False}
+
+
+class TopologicalSorter(object):
+    # created this class because we don't want to mutate the graph itself during the topological sorting algorithm
+    def __init__(self, graph):
+        self.nodes = {}
+        for key, dct in graph.nodes.items():
+            self.nodes[key] = {'edges_to':dct['edges_to'], 'temp_mark':False, 'perm_mark':not dct['added']}
+        self.output = []
+
+    def sort(self):
+        for node in self.nodes:
+            self.visit(node)
+        self.output.reverse()
+        return self.output
+
+    def visit(self, node):
+        node_data = self.nodes[node]
+        if node_data['perm_mark']:
+            return
+        if node_data['temp_mark']:
+            raise ValueError('Graph has cycles')
+
+        node_data['temp_mark'] = True
+
+        for m in node_data['edges_to']:
+            self.visit(m)
+
+        self.output.append(node)
+        node_data['perm_mark'] = True
+
 
