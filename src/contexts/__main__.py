7dcbb064e9bd87e30d322e695452ab140c30b5ed
Benjamin Hodgson <benjamin.hodgson@huddle.com>
2014-03-31 21:14:03 +0100

    Support for --version

--- src/contexts/__main__.py
+++ src/contexts/__main__.py
@@ -4,6 +4,10 @@ from . import run_with_plugins, main
 
 
 def cmd():
+    if '--version' in sys.argv:
+        print_version()
+        sys.exit(0)
+
     try:
         import colorama
     except ImportError:
@@ -14,6 +18,16 @@ def cmd():
     plugin_list = load_plugins()
     exit_code = run_with_plugins(plugin_list)
     sys.exit(exit_code)
+
+
+def print_version():
+    import pkg_resources
+    version = pkg_resources.require('contexts')[0].version
+    py_version = '.'.join(str(i) for i in sys.version_info[0:3])
+
+    print("Contexts version " + version)
+    print("Running on Python version " + py_version)
+
 
 
 if __name__ == "__main__":
