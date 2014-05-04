9fad35f89dfe5e48ce84c7fd62737e681df7d2c5
Benjamin Hodgson <benjamin.hodgson@huddle.com>
2014-05-04 18:21:19 +0100

    improving the package

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import setup, find_packages
 
 
 builtin_plugins = [
-    'PathSupplier = contexts.plugins.test_target_suppliers:PathSupplier',
+    'CommandLineSupplier = contexts.plugins.test_target_suppliers:CommandLineSupplier',
     'ExitCodeReporter = contexts.plugins.reporting:ExitCodeReporter',
     'Shuffler = contexts.plugins.shuffling:Shuffler',
     'Importer = contexts.plugins.importing:Importer',
@@ -25,19 +25,33 @@ builtin_plugins = [
 
 
 setup(
-    name = "Contexts",
-    version = "0.9.2",
-    author = "Benjamin Hodgson",
-    author_email = "benjamin.hodgson@huddle.net",
-    url = "https://github.com/benjamin-hodgson/Contexts",
-    description = """Dead simple descriptive testing for Python. No custom decorators, no context managers, no '.feature' files, no fuss.""",
-    long_description = """See the Github project page (https://github.com/benjamin-hodgson/Contexts) for more information.""",
-    package_dir = {'':'src'},
-    packages = find_packages('src'),
-    install_requires = ["setuptools >= 1.0"],
-    extras_require = {'colour': ["colorama >= 0.2.7"]},
-    entry_points = {
-    	'console_scripts': ['run-contexts = contexts.__main__:cmd'],
+    name="Contexts",
+    version="0.9.2",
+    author="Benjamin Hodgson",
+    author_email="benjamin.hodgson@huddle.net",
+    url="https://github.com/benjamin-hodgson/Contexts",
+    description="""Dead simple descriptive testing for Python. No custom decorators, no context managers, no '.feature' files, no fuss.""",
+    long_description="""See the Github project page (https://github.com/benjamin-hodgson/Contexts) for more information.""",
+    package_dir={'': 'src'},
+    packages=find_packages('src'),
+    install_requires=["setuptools >= 1.0"],
+    extras_require={'colour': ["colorama >= 0.2.7"]},
+    entry_points={
+        'console_scripts': ['run-contexts=contexts.__main__:cmd'],
         'contexts.plugins': builtin_plugins,
-    }
+    },
+    classifiers=[
+        "Development Status :: 4 - Beta",
+        "Programming Language :: Python",
+        "Programming Language :: Python :: 3.3",
+        "Programming Language :: Python :: 3.4",
+        "License :: OSI Approved :: MIT License",
+        "Intended Audience :: Developers",
+        "Intended Audience :: Information Technology",
+        "Natural Language :: English",
+        "Operating System :: OS Independent",
+        "Topic :: Software Development",
+        "Topic :: Software Development :: Quality Assurance",
+        "Topic :: Software Development :: Testing"
+    ]
 )
