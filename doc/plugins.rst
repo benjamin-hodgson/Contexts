72db97ce7e572e2a36e58712682df4322436d42d
Benjamin Hodgson <benjamin.hodgson@huddle.com>
2014-09-27 15:32:12 +0100

    Document plugin interface

--- doc/plugins.rst
+++ doc/plugins.rst
@@ -7,8 +7,8 @@ Contexts features an experimental 'plugin' interface for user-customisable behav
 
 The plugin interface
 --------------------
-For documentation purposes, the full plugin interface is defined in :class:`PluginInterface`.
-Currently, plugins are **required** to implement the :meth:`initialise <PluginInterface.initialise>` method (to determine whether the plugin is active in the current test run). The rest of the plugin interface is optional.
+For documentation purposes, the full plugin interface is defined in :class:`~contexts.plugin_interface.PluginInterface`.
+Currently, plugins are **required** to implement the :meth:`~contexts.plugin_interface.PluginInterface.initialise` method (to determine whether the plugin is active in the current test run). The rest of the plugin interface is optional.
 
 Contexts's plugin support is implemented as an ordered list of plugin classes. Each time a plugin hook is called, each plugin is called in turn. Plugins which do not implement the hook are skipped. The *first* return value is used as the return value of the aggregated plugin calls - when a plugin returns a value from a hook, all the remaining plugins in the list are skipped. This means that a given plugin is able to override the behaviour of plugins which follow it in the list.
 
@@ -17,18 +17,18 @@ Contexts's plugin support is implemented as an ordered list of plugin classes. E
 
 The plugin lifecycle
 --------------------
-Because plugins can override one another, the ordering of the list matters. The interface defines a :func:`classmethod <classmethod>` entitled :meth:`locate <PluginInterface.locate>`, which you can implement to insert your plugin before or after another plugin.
+Because plugins can override one another, the ordering of the list matters. The interface defines a :func:`classmethod <classmethod>` entitled :meth:`~contexts.plugin_interface.PluginInterface.locate`, which you can implement to insert your plugin before or after another plugin.
 
-Plugins are given control over whether or not they appear in the list. All plugins **must** define an :meth:`initialise(args, environ) <PluginInterface.initialise>` method, and return either ``True`` or ``False`` to signal whether they want to appear in the list. You may also define a :meth:`setup_parser(parser) <PluginInterface.setup_parser>` method to modify the :class:`argparse.ArgumentParser` instance that is used to parse command-line arguments.
+Plugins are given control over whether or not they appear in the list. All plugins **must** define an :meth:`initialise(args, environ) <contexts.plugin_interface.PluginInterface.initialise>` method, and return either ``True`` or ``False`` to signal whether they want to appear in the list. You may also define a :meth:`setup_parser(parser) <contexts.plugin_interface.PluginInterface.setup_parser>` method to modify the :class:`argparse.ArgumentParser` instance that is used to parse command-line arguments.
 
-Very occasionally, it is necessary for plugins to modify the behaviour of other plugin objects (see :class:`FailuresOnly` for an example) - you can define a :meth:`request_plugins <PluginInterface.request_plugins>` generator method to request the current instances of some other plugin classes from the test runner.
+Very occasionally, it is necessary for plugins to modify the behaviour of other plugin objects (see :class:`FailuresOnly` for an example) - you can define a :meth:`~contexts.plugin_interface.PluginInterface.request_plugins` generator method to request the current instances of some other plugin classes from the test runner.
 
 
 .. _progress:
 
 Progress notifications
 ----------------------
-There exist a number of plugin hooks which are called when progress through the test run reaches certain points. These methods include :meth:`test_run_started <PluginInterface.test_run_started>`, :meth:`context_ended <PluginInterface.context_ended>`, :meth:`assertion_failed <PluginInterface.assertion_failed>`, and so on.
+There exist a number of plugin hooks which are called when progress through the test run reaches certain points. These methods include :meth:`~contexts.plugin_interface.PluginInterface.test_run_started`, :meth:`~contexts.plugin_interface.PluginInterface.context_ended`, :meth:`~contexts.plugin_interface.PluginInterface.assertion_failed`, and so on.
 
 These hooks are typically used to report progress to the user. It's not recommended to return a value from these methods, unless you want to prevent other plugins from being told about the progress.
 
@@ -37,9 +37,9 @@ These hooks are typically used to report progress to the user. It's not recommen
 
 Identifying test objects
 ------------------------
-When the test runner sees a file, folder, class, or method, it queries the list of plugins to find out whether it should run it, using the :meth:`identify_folder <PluginInterface.identify_folder>`, :meth:`identify_file <PluginInterface.identify_file>`, :meth:`identify_class <PluginInterface.identify_class>`, and :meth:`identify_method <PluginInterface.identify_method>` hooks. The expected return values from these methods are defined as constants in the :mod:`plugin_interface` module: :const:`TEST_FILE <plugin_interface.TEST_FILE>`, :const:`CONTEXT <plugin_interface.CONTEXT>`, :const:`ASSERTION <plugin_interface.ASSERTION>` and so on.
+When the test runner sees a file, folder, class, or method, it queries the list of plugins to find out whether it should run it, using the :meth:`~contexts.plugin_interface.PluginInterface.identify_folder`, :meth:`~contexts.plugin_interface.PluginInterface.identify_file`, :meth:`~contexts.plugin_interface.PluginInterface.identify_class`, and :meth:`~contexts.plugin_interface.PluginInterface.identify_method` hooks. The expected return values from these methods are defined as constants in the :mod:`contexts.plugin_interface` module: :const:`~contexts.plugin_interface.TEST_FILE`, :const:`~contexts.plugin_interface.CONTEXT`, :const:`~contexts.plugin_interface.ASSERTION` and so on.
 
-After all the modules have been imported, the :meth:`process_module_list <PluginInterface.process_module_list>` hook is called, which plugins can use to inject their own test modules, or remove modules that should not be run. There are also :meth:`process_class_list <PluginInterface.process_class_list>` and :meth:`process_assertion_list <PluginInterface.process_assertion_list>` hooks.
+After all the modules have been imported, the :meth:`~contexts.plugin_interface.PluginInterface.process_module_list` hook is called, which plugins can use to inject their own test modules, or remove modules that should not be run. There are also :meth:`~contexts.plugin_interface.PluginInterface.process_class_list` and :meth:`~contexts.plugin_interface.PluginInterface.process_assertion_list` hooks.
 
 
 .. _other:
@@ -65,3 +65,38 @@ Once you've written your plugin, you can register it with Contexts using the ``c
         # ...
     )
 
+
+The plugin API
+--------------
+
+.. automodule:: contexts.plugin_interface
+
+.. autoclass:: PluginInterface
+    :members:
+
+.. autodata:: TEST_FOLDER
+    :annotation:
+
+.. autodata:: TEST_FILE
+    :annotation:
+
+.. autodata:: CONTEXT
+    :annotation:
+
+.. autodata:: EXAMPLES
+    :annotation:
+
+.. autodata:: SETUP
+    :annotation:
+
+.. autodata:: ACTION
+    :annotation:
+
+.. autodata:: ASSERTION
+    :annotation:
+
+.. autodata:: TEARDOWN
+    :annotation:
+
+.. autodata:: NO_EXAMPLE
+    :annotation:
--- src/contexts/plugin_interface.py
+++ src/contexts/plugin_interface.py
@@ -1,43 +1,44 @@
 class PluginInterface(object):
     """
-    Interface for plugins.
+    Defines the interface for plugins.
+
+    You do not need to inherit from this class in your own plugins.
+    Just create a new class and implement `initialise` (and one or more other hooks).
     """
     @classmethod
     def locate(cls):
         """
         Called before the plugin is instantiated, to determine where it should
         appear in the list of plugins.
-        The ordering of this list matters. If a plugin returns a (non-None) value
+        The ordering of this list matters. If a plugin returns a (non-``None``) value
         from a given method, plugins later in the list will not get called.
 
-        Plugins may return a 2-tuple of (`left`, `right`).
-        Here, `left` is a plugin class which this plugin wishes to _follow_,
-        and `right` is a class the plugin wishes to _precede_.
-        Either or both of the values may be None, to indicate that the plugin
+        Plugins may return a 2-tuple of ``(left, right)``.
+        Here, ``left`` is a plugin class which this plugin wishes to *follow*,
+        and ``right`` is a class the plugin wishes to *precede*.
+        Either or both of the values may be `None`, to indicate that the plugin
         does not mind what it comes before or after, respectively.
-        Returning None from this method is equivalent to returning (None, None).
+        Returning ``None`` from this method is equivalent to returning ``(None, None)``.
         """
     def setup_parser(self, parser):
         """
         Called before command-line arguments are parsed.
 
-        Plugins may mutate `parser`, which is an instance of
-        `argparse.ArgumentParser`, in order to set it up to expect the options
-        the plugin needs to configure itself. See the standard library documentation
-        for `argparse` for more information.
+        :param parser: An instance of :class:`argparse.ArgumentParser`.
+            Plugins may mutate ``parser`` in order to set it up to expect the options
+            the plugin needs to configure itself. See the standard library documentation
+            for :mod:`argparse` for more information.
         """
     def initialise(self, args, environ):
         """
         Called after command-line arguments are parsed.
 
-        `args` is the result of `ArgumentParser.parse_args()`.
-        (see the standard library documentation for `argparse` for more information).
-        `environ` is the value of `os.environ`.
-        Plugins should use these to set themselves up.
+        :param args: The result of :meth:`ArgumentParser.parse_args <argparse.ArgumentParser.parse_args>`.
+            (see the standard library documentation for :mod:`argparse` for more information).
+        :param environ: The value of :data:`os.environ`.
 
-        Plugins may return True or False from this method. Returning True will cause
-        the plugin to be added to the list of plugins for this test run. Returning False
-        will prevent this.
+        :return: A boolean. Returning ``True`` will cause the plugin to be added to the list of plugins
+            for this test run. Returning ``False`` will prevent this.
         """
     def request_plugins(self):
         """
@@ -53,21 +54,21 @@ class PluginInterface(object):
         """
 
     def test_run_started(self):
-        """Called at the beginning of a test run"""
+        """Called at the beginning of a test run."""
     def test_run_ended(self):
-        """Called at the end of a test run"""
+        """Called at the end of a test run."""
 
     def suite_started(self, module):
         """
         Called at the start of a test module.
 
-        `module` is the Python module (an instance of `types.ModuleType`) that is about to be run.
+        :param module: The Python module (an instance of :class:`~types.ModuleType`) that is about to be run.
         """
     def suite_ended(self, module):
         """
         Called at the end of a test module.
 
-        `module` is the Python module (an instance of `types.ModuleType`) that was just run.
+        :param module: The Python module (an instance of :class:`~types.ModuleType`) that was just run.
         """
 
     def test_class_started(self, cls):
@@ -77,103 +78,125 @@ class PluginInterface(object):
         A test class may contain one or more test contexts.
         (Test classes with examples will generally contain more than one.)
 
-        `cls` is the class object that is being run.
+        :param cls: The class object that is being run.
         """
     def test_class_ended(self, cls):
         """
         Called when a test class ends its run.
 
-        `cls` is the class object that is being run.
+        :param cls: The class object that is being run.
         """
     def test_class_errored(self, cls, exception):
         """
         Called when a test class unexpectedly errors.
 
-        `cls` is the class object that is being run. `exception` is the exception that got thrown.
+        :param cls: The class object that is being run.
+        :param exception: The exception that got caused the error.
         """
 
     def context_started(self, cls, example):
         """
         Called when a test context begins its run.
 
-        `cls` is the class object of the test being run. `example` is the current example,
-        which may be contexts.plugin_interface.NO_EXAMPLE if it is not a parametrised test.
+        :param cls: The class object of the test being run.
+        :param example: The current example, which may be :const:`~contexts.plugin_interface.NO_EXAMPLE`
+            if it is not a parametrised test.
         """
     def context_ended(self, cls, example):
         """
-        Called when a test context completes its run
+        Called when a test context completes its run.
 
-        `cls` is the class object of the test being run. `example` is the current example,
-        which may be contexts.plugin_interface.NO_EXAMPLE if it is not a parametrised test.
+        :param cls: The class object of the test being run.
+        :param example: The current example, which may be :const:`~contexts.plugin_interface.NO_EXAMPLE`
+            if it is not a parametrised test.
         """
     def context_errored(self, cls, example, exception):
         """
-        Called when a test context (not an assertion) throws an exception
+        Called when a test context (not an assertion) throws an exception.
 
-        `cls` is the class object of the test being run. `example` is the current example,
-        which may be contexts.plugin_interface.NO_EXAMPLE if it is not a parametrised test.
-        `exception` is the exception which got thrown.
+        :param cls: The class object of the test being run.
+        :param example: The current example, which may be :const:`~contexts.plugin_interface.NO_EXAMPLE`
+            if it is not a parametrised test.
+        :param exception: The exception that caused the error.
         """
 
     def assertion_started(self, name):
-        """Called when an assertion begins"""
+        """
+        Called when an assertion begins.
+
+        :param name str: The name of the assertion method being run.
+        """
     def assertion_passed(self, name):
-        """Called when an assertion passes"""
+        """
+        Called when an assertion passes.
+
+        :param name str: The name of the assertion method being run.
+        """
     def assertion_errored(self, name, exception):
-        """Called when an assertion throws an exception"""
+        """
+        Called when an assertion throws an exception.
+
+        :param name str: The name of the assertion method being run.
+        :param exception: The exception that caused the error.
+        """
     def assertion_failed(self, name, exception):
-        """Called when an assertion throws an AssertionError"""
+        """
+        Called when an assertion throws an AssertionError.
+
+        :param name str: The name of the assertion method being run.
+        :param exception: The exception that caused the failure.
+        """
 
     def unexpected_error(self, exception):
-        """Called when an error occurs outside of a Context or Assertion"""
+        """
+        Called when an error occurs outside of a Context or Assertion.
+
+        :param exception: The exception that caused the failure.
+        """
 
     def get_object_to_run(self):
         """
         Called before the start of the test run, when the test runner wants to know
         what it should run.
 
-        Plugins may return:
-            a class - the test runner will run the identified methods in this class
-            a file path - the test runner will run the identified classes in this file
-            a folder path - the test runner will run the identified
-                            files and subfolders in this folder
-            None - the plugin doesn't want to choose what to run
+        This method should return one of:
+            * a class - the test runner will run the identified methods in this class.
+            * a file path as a string - the test runner will run the identified classes in this file.
+            * a folder path as a string - the test runner will run the identified files and subfolders in this folder.
+            * ``None`` - the plugin doesn't want to choose what to run.
         """
     def identify_folder(self, folder):
         """
         Called when the test runner encounters a folder and wants to know if it should
-        run the files in that folder.
+        run the tests in that folder.
 
-        Arguments:
-            folder - the full path of the folder which the test runner wants to be identified
+        :param folder str: The full path of the folder which the test runner wants to be identified
 
-        Plugins may return:
-            contexts.plugin_interface.TEST_FOLDER - plugin wishes the folder to be treated as a test folder
-            None - plugin does not wish to identify the folder (though other plugins may still cause it to be run)
+        This method should return one of:
+            * :const:`~contexts.plugin_interface.TEST_FOLDER` - plugin wishes the folder to be treated as a test folder
+            * ``None`` - plugin does not wish to identify the folder (though other plugins may still cause it to be run)
         """
     def identify_file(self, file):
         """
         Called when the test runner encounters a file and wants to know if it should
-        run the files in that file.
+        run the tests in that file.
 
-        Arguments:
-            file - the full path of the file which the test runner wants to be identified
+        :param file str: The full path of the file which the test runner wants to be identified.
 
-        Plugins may return:
-            contexts.plugin_interface.TEST_FILE - plugin wishes the file to be imported and run as a test file
-            None - plugin does not wish to identify the file (though other plugins may still cause it to be run)
+        This method should return one of:
+            * :const:`~contexts.plugin_interface.TEST_FILE` - plugin wishes the file to be imported and run as a test file
+            * ``None`` - plugin does not wish to identify the file (though other plugins may still cause it to be run)
         """
     def identify_class(self, cls):
         """
         Called when the test runner encounters a class and wants to know if it should
         treat it as a test class.
 
-        Arguments:
-            cls - the class object which the test runner wants to be identified.
+        :param cls: The class object which the test runner wants to be identified.
 
-        Plugins may return:
-            contexts.plugin_interface.CONTEXT - plugin wishes the class to be treated as a test class
-            None - plugin does not wish to identify the class (though other plugins may still cause it to be run)
+        This method should return one of:
+            * :const:`~contexts.plugin_interface.CONTEXT` - plugin wishes the class to be treated as a test class
+            * ``None`` - plugin does not wish to identify the class (though other plugins may still cause it to be run)
         """
     def identify_method(self, func):
         """
@@ -182,62 +205,80 @@ class PluginInterface(object):
 
         When a test class has a superclass, all the superclass's methods will be passed in first.
 
-        Arguments:
-            func - the unbound method (or bound classmethod) which the test runner wants to be identified
+        :param func: The unbound method (or bound classmethod) which the test runner wants to be identified
 
-        Plugins may return:
-            contexts.plugin_interface.EXAMPLES - plugin wishes the method to be treated as an 'examples' method
-            contexts.plugin_interface.SETUP - plugin wishes the method to be treated as an 'establish' method
-            contexts.plugin_interface.ACTION - plugin wishes the method to be treated as a 'because'
-            contexts.plugin_interface.ASSERTION - plugin wishes the method to be treated as an assertion method
-            contexts.plugin_interface.TEARDOWN - plugin wishes the method to be treated as a teardown method
-            None - plugin does not wish to identify the method (though other plugins may still cause it to be run)
+        This method should return one of:
+            * :const:`~contexts.plugin_interface.EXAMPLES` - plugin wishes the method to be treated as an 'examples' method
+            * :const:`~contexts.plugin_interface.SETUP` - plugin wishes the method to be treated as an 'establish' method
+            * :const:`~contexts.plugin_interface.ACTION` - plugin wishes the method to be treated as a 'because'
+            * :const:`~contexts.plugin_interface.ASSERTION` - plugin wishes the method to be treated as an assertion method
+            * :const:`~contexts.plugin_interface.TEARDOWN` - plugin wishes the method to be treated as a teardown method
+            * ``None`` - plugin does not wish to identify the method (though other plugins may still cause it to be run)
         """
 
     def process_module_list(self, modules):
-        """Called with the full list of found modules. Plugins may modify the list in-place."""
+        """
+        A hook to change (or examine) the list of modules which will be run with the full list of found modules.
+        Plugins may modify the list in-place by adding or removing modules.
+
+        :param modules: A list of :class:`types.ModuleType`.
+        """
     def process_class_list(self, module, classes):
         """
-        Called with the list of classes found in a module.
-        Plugins may modify the list in-place.
+        A hook to change (or examine) the list of classes found in a module.
+        Plugins may modify the list in-place by adding or removing classes.
 
-        Arguments:
-            module - the Python module in which the classes were found
-            classes - the classes found in that module
+        :param module: The Python module in which the classes were found (an instance of :class:`types.ModuleType`).
+        :param classes: A list of classes found in that module.
         """
     def process_assertion_list(self, cls, functions):
         """
-        Called with the list of (unbound) assertion methods found in a class.
-        Plugins may modify the list in-place.
+        A hook to change (or examine) the list of (unbound) assertion methods found in a class.
+        Plugins may modify the list in-place by adding or removing functions.
 
-        Arguments:
-            cls - the test class in which the methods were found
-            functions - the unbound assertion methods found in that class
+        :param cls: The test class in which the methods were found
+        :param functions: A list of unbound assertion methods found in that class
         """
 
     def import_module(self, location, name):
         """
-        Called when the test runner wants a module imported.
-        Plugins may return an imported module object, or None if they do not want to import the module.
+        Called when the test runner needs to import a module.
 
         Arguments:
             location: string. Path to the folder containing the module or package.
             name: string. Full name of the module, including dot-separated package names.
+
+        This method should return one of:
+            * an imported module (an instance of :class:`types.ModuleType`).
+                This may be a reference to an existing module, or a "fake" generated module.
+            * ``None``, if the plugin is not able to import the module.
         """
 
     def get_exit_code(self):
         """
         Called at the end of the test runner to obtain the exit code for the process.
-        Plugins may return an integer, or None if they do not want to override the default behaviour.
+
+        This method should return one of:
+            * An integer
+            * ``None``, if you do not want to override the default behaviour.
         """
 
 
+#: Returned by plugins to indicate that a folder contains tests.
 TEST_FOLDER = type("_TestFolder", (), {})()
+#: Returned by plugins to indicate that a file contains tests.
 TEST_FILE = type("_TestFile", (), {})()
+#: Returned by plugins to indicate that a class is a test class.
 CONTEXT = type("_Context", (), {})()
+#: Returned by plugins to indicate that a method is an Examples method.
 EXAMPLES = type("_Examples", (), {})()
+#: Returned by plugins to indicate that a method is a setup method.
 SETUP = type("_Setup", (), {})()
+#: Returned by plugins to indicate that a method is an action method.
 ACTION = type("_Action", (), {})()
+#: Returned by plugins to indicate that a method is an assertion method.
 ASSERTION = type("_Assertion", (), {})()
+#: Returned by plugins to indicate that a method is a teardown method.
 TEARDOWN = type("_Teardown", (), {})()
+#: Passed to plugins when a class is not a parametrised test.
 NO_EXAMPLE = type("_NoExample", (), {})()
