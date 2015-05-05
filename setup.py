6910e15fc8016750f5f9a1038eda17872a3e58d3
Benjamin Hodgson <benjamin.hodgson@huddle.com>
2015-05-05 12:08:57 +0100

    Increment version to 0.10.2

--- setup.py
+++ setup.py
@@ -26,7 +26,7 @@ builtin_plugins = [
 
 setup(
     name="Contexts",
-    version="0.10.1",
+    version="0.10.2",
     author="Benjamin Hodgson",
     author_email="benjamin.hodgson@huddle.net",
     url="https://github.com/benjamin-hodgson/Contexts",
