7895714c41a6e9050ad56cb562fa09bc31f1e760
Benjamin Hodgson <benjamin.hodgson@huddle.com>
2015-09-16 01:32:10 +0100

    travis and pypi 3.5

--- setup.py
+++ setup.py
@@ -46,6 +46,7 @@ setup(
         "Programming Language :: Python",
         "Programming Language :: Python :: 3.3",
         "Programming Language :: Python :: 3.4",
+        "Programming Language :: Python :: 3.5",
         "License :: OSI Approved :: MIT License",
         "Intended Audience :: Developers",
         "Intended Audience :: Information Technology",
